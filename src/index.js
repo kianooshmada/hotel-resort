73f571a9ebd4b20b3bf4cb56cae407abbf2b3ccb
kianooshmada <kianooshnike1365@gmail.com>
2021-06-21 13:32:50 +0430

    third

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { RoomProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
